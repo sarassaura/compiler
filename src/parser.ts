828d98daf4935738234a8dd4abaa0520e5f6c5f8
sarassaura <sarahyukinonakada@gmail.com>
2023-12-29 21:03:24 -0300

    Error Handling

--- src/parser.ts
+++ src/parser.ts
@@ -1,8 +1,16 @@
 import lexer from './lexer';
 
 export default function parser(str: string): AST {
+	const Tokens = lexer(str);
+
+	Tokens.forEach((token) => {
+		if (token.kind == 'INVALID') {
+			console.log('Invalid Character: ', token.value);
+		}
+	});
+
 	return {
 		type: 'program',
-		body: lexer(str)
+		body: Tokens
 	};
 }
